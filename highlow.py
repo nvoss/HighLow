6b5ee0819d2bce30b8fd5dfd284b2bfa4aa9d0f5
Nathan Voss <nathancvoss@gmail.com>
2017-06-12 11:59:14 -0500

    Tuning

--- highlow.py
+++ highlow.py
@@ -5,8 +5,7 @@ app = Flask(__name__, static_url_path='')
 
 
 level_1_secret = 0
-level_2_secret = 0
-level_2_guesses = 10
+level_1_guesses = 10
 attempts = 0
 
 
@@ -27,30 +26,30 @@ def attempt_a_guess(guess, secret, flag):
 
 @app.route('/level1/<guess>')
 def level_1_guess(guess):
-    return attempt_a_guess(int(guess), level_1_secret, "FLAG{PrIce_is_RighT}")
-
-
-@app.route('/level2/<guess>')
-def level_2_guess(guess):
-    global level_2_guesses
-    if level_2_guesses > 0:
-        level_2_guesses -= 1
-        return jsonify(attempt_a_guess(int(guess), level_2_secret, "FLAG{Pr3s5_YoUr_lUck}"), str(level_2_guesses))
+    global level_1_guesses
+    if level_1_guesses > 0:
+        level_1_guesses -= 1
+        return jsonify(attempt_a_guess(int(guess), level_1_secret, "FLAG{Pr3s5_YoUr_lUck}"), str(level_1_guesses))
     else:
         return jsonify("Game Over", "0")
 
 
 @app.route('/level1')
 def level1():
+    global level_1_secret
+    global level_1_guesses
+    level_1_guesses = 10
+    level_1_secret = generate_random_number(1000)
     return current_app.send_static_file('level1.html')
+
+
+@app.route('/level2/flag')
+def level2_flag():
+    return current_app.send_static_file('flag2.png')
 
 
 @app.route('/level2')
 def level2():
-    global level_2_secret
-    global level_2_guesses
-    level_2_guesses = 10
-    level_2_secret = generate_random_number(1000)
     return current_app.send_static_file('level2.html')
 
 
